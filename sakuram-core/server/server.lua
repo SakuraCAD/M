aae4adbfdca4cd2791e4e833dfc1e657ddb36359
Friz <frizjack2017@gmail.com>
2020-05-27 23:37:29 +0100

    Updated Server Information File

--- sakuram-core/server/server.lua
+++ sakuram-core/server/server.lua
@@ -4,6 +4,7 @@ RegisterNetEvent("call911")
 RegisterNetEvent("911CallReturn")
 
 
+
 function Return911()
     TriggerClientEvent("911CallReturn")
 end
@@ -25,6 +26,8 @@ AddEventHandler("call911", function(player, calldata)
     local reason = calldata.reason
     local coords = calldata.coords
     local testDiscordWebhook = "https://discordapp.com/api/webhooks/715224849500471407/nPW6sR56dyUg9YeBPMxuh3H3pVE9R1fNwT2A9_Wmlu6MpXCNtxK9O_zLGA5xLoqQst3I"
-    local data = json.encode({content = "**Crime in Progress**\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
+    local data = json.encode({content = "**Crime in Progress**\nAuthority Requested :" .. authority .. "\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
     PerformHttpRequest(testDiscordWebhook, returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
 end)
+
+print(convarValue)
