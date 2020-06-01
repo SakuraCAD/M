4960264c016caf722ba15e18eb754fbd1ee0ce0b
Friz <frizjack2017@gmail.com>
2020-06-01 01:55:27 +0100

    Remove Security Incident

--- sakuram-core/server/server.lua
+++ sakuram-core/server/server.lua
@@ -11,9 +11,6 @@ end
 -- callback 
 
 function returnDiscord(player,errorCode, resultData, resultHeaders)
-    print("Returned error code:" .. tostring(errorCode))
-    print("Returned data:" .. tostring(resultData))
-    print("Returned result Headers:" .. tostring(resultHeaders))
     Return911(player,errorCode)
 end
 
@@ -25,9 +22,8 @@ AddEventHandler("call911", function(player, calldata)
     local location = calldata.location
     local reason = calldata.reason
     local coords = calldata.coords
-    local testDiscordWebhook = "https://discordapp.com/api/webhooks/715224849500471407/nPW6sR56dyUg9YeBPMxuh3H3pVE9R1fNwT2A9_Wmlu6MpXCNtxK9O_zLGA5xLoqQst3I"
+    local testDiscordWebhook = "removed_for_security_reasons"
     local data = json.encode({content = "**Crime in Progress**\nAuthority Requested :" .. authority .. "\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
     PerformHttpRequest(testDiscordWebhook, returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
 end)
 
-print(convarValue)
