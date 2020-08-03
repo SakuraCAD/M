efb7976d08f258b63170877efc3f29e9d2707ea0
Lewis Crichton <lewistehminerz@outlook.com>
2020-08-03 16:33:59 +0100

    horizon stuff

--- sakuram-core/fxmanifest.lua
+++ sakuram-core/fxmanifest.lua
@@ -6,7 +6,10 @@ author 'SakuraCAD'
 description 'SakuraCAD Server Plugin for FiveM'
 version '1.0.0'
 
+dependency 'yarn'
+
 -- What to run
 client_script 'client/client.lua'
 
+server_script 'server/horizon.js'
 server_script 'server/server.lua'
---
+++ sakuram-core/server/horizon.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+const { v4: uuid } = require('uuid');
+
+const wsUrl = 'wss://api.sakuracad.app/';
+const socket = new WebSocket(wsUrl);
+
+const cbMap = new Map();
+
+on('sakuram:horizon:send', (operation, data, callback) => {
+    let r = null;
+
+    if (callback) {
+        r = uuid();
+        cbMap.set(r, callback);
+    }
+
+    socket.send(JSON.stringify({ r, o: operation, d: data }));
+});
+
+socket.on('message', data => {
+    const dataReceived = JSON.parse(data);
+
+    if (dataReceived.r && cbMap.has(dataReceived.r)) {
+        cbMap.get(dataReceived.r)();
+        cbMap.delete(dataReceived.r);
+    }
+});
