ff326355f6dc3c385f119b3f85bea11b1c7fe3f2
Friz <frizjack2017@gmail.com>
2020-08-03 13:45:49 +0100

    Major Fix

--- sakuram-core/client/client.lua
+++ sakuram-core/client/client.lua
@@ -8,25 +8,24 @@ RegisterCommand('911',function(source,args)
     local playerPed = PlayerPedId(-1)
     local coords = GetEntityCoords(playerPed)
     local locationStreet = GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(playerPed))))
-    local authoritiesCalled = string.lower(args[1])
+    print("Preparing Call for Player Located At " .. locationStreet)
     for index, argument in pairs(args) do
-        if index ~= 1 then 
-            table.insert(strings,index,argument)
-        end
+        table.insert(strings,index,argument)
     end
+    local authoritiesCalled = string.lower(strings[1])
     local callReason = table.concat(strings," ")
-
-
-    if authoritiesCalled ~= "police" or authoritiesCalled ~= "ems" or authoritiesCalled ~= "fire" then
+    if string.find(authoritiesCalled,"police") or string.find(authoritiesCalled,"ems") or string.find(authoritiesCalled,"fire")  then
+        local data = {authorities = authoritiesCalled, location = locationStreet, reason = callReason, coords = {x = coords.x, y = coords.y, z = coords.z}}
+        TriggerServerEvent("call911", playerPed, data)
+        TriggerEvent('chat:addMessage',{
+            args = {'Dialling 911 to request '.. authoritiesCalled}
+        })
+    else
         TriggerEvent('chat:addMessage',{
             args = {'Please select an emergency service using the options : police | ems | fire'}
         })
-    else
-        TriggerServerEvent("call911", playerPed, {authorities = authoritiesCalled, location = locationStreet, reason = callReason, coords = {x = coords.x, y = coords.y, z = coords.z}})
-        TriggerEvent('chat:addMessage',{
-            args = {'Dialling 911'}
-        })
     end
+    strings = {}
 end,false)
 
 -- Handle Return Event from Server
--- sakuram-core/server/server.lua
+++ sakuram-core/server/server.lua
@@ -5,8 +5,8 @@ RegisterNetEvent("911CallReturn")
 
 
 -- Return 911 Call
-function Return911()
-    TriggerClientEvent("911CallReturn")
+function Return911(player,error)
+    TriggerClientEvent("911CallReturn", player, error)
 end
 
 -- Callback
@@ -18,12 +18,12 @@ end
 
 -- Handle Client 911 Calls
 AddEventHandler("call911", function(player, calldata)
-    local authority = calldata.authority
+    local authority = calldata.authorities
+    print(authority)
     local location = calldata.location
     local reason = calldata.reason
     local coords = calldata.coords
-    local url = ""
-    local data = json.encode({content = "**Crime in Progress**\nAuthority Requested :" .. authority .. "\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
-    PerformHttpRequest(url, returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
+    local data = json.encode({content = "@everyone\n**Crime in Progress**\nAuthority Requested :" .. authority .. "\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
+    print("preparing to fire html")
+    PerformHttpRequest("https://discordapp.com/api/webhooks/739813592890736742/O2bYksber1Yep8eB0vE3rsHBRnGhnjRzZfJvSw8_A9DgJUqTmybSethbGC4b_1NOypGJ", returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
 end)
-
