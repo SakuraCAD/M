6c927aa5afc9f7e22dad76885eaeb98ff66fe4e1
Friz <frizjack2017@gmail.com>
2020-08-03 15:51:56 +0100

    Removing Webhook

--- sakuram-core/server/server.lua
+++ sakuram-core/server/server.lua
@@ -25,5 +25,5 @@ AddEventHandler("call911", function(player, calldata)
     local coords = calldata.coords
     local data = json.encode({content = "@everyone\n**Crime in Progress**\nAuthority Requested :" .. authority .. "\nStreet : " .. location .. "\nCoordinates (X, Y and Z) : `" .. coords.x .. " | "..coords.y .. "| " .. coords.z .. "`\nCaller Information : \n```\n" .. reason .. "\n```"})
     print("preparing to fire html")
-    PerformHttpRequest("https://discordapp.com/api/webhooks/739813592890736742/O2bYksber1Yep8eB0vE3rsHBRnGhnjRzZfJvSw8_A9DgJUqTmybSethbGC4b_1NOypGJ", returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
+    PerformHttpRequest("", returnDiscord(player), 'POST', data, {["Content-Type"] = "application/json"})
 end)
