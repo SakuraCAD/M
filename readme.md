414186a8bc0bf05950b84c12b5d60e6e12a603d4
Friz <frizjack2017@gmail.com>
2020-08-04 01:20:17 +0100

    Implement UI Library and Credit Creator
    Adds UI Library and implements Credits for Creator in Readme

--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ SakuraM is the FiveM Plugin Component of the Sakura Computer Aided Dispatch Syst
 All of our documentation is stored on Gitbooks. Find out more here
 
 [![Gitbook Logo](https://gblobscdn.gitbook.com/spaces%2Fgitbook%2Favatar-rectangle.png?alt=media)](https://docs.sakuracad.app/setup/sakuram)
+
+# Open Source Credits
+
+[![ReadMe Card](https://github-readme-stats.vercel.app/api/pin/?username=Sheamle&repo=notif&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
--- sakuram-core/client/client.lua
+++ sakuram-core/client/client.lua
@@ -8,6 +8,8 @@ ____/ // /_/ /_  ,<  / /_/ /_  /   / /_/ // /___  _  ___ |  /_/ /
     Developed by Friz#8136 and LewisTehMinerz#1337
     https://sakuracad.app
     Made using Visual Studio Code - Insiders
+
+    Open Source Credits : Sheamle/notif (Notifications Library)
 ]]--
 
 -- Setup Convars and Events
@@ -16,6 +18,146 @@ ____/ // /_/ /_  ,<  / /_/ /_  /   / /_/ // /___  _  ___ |  /_/ /
 RegisterNetEvent("call911")
 RegisterNetEvent("911CallReturn")
 
+-- LIBRARY : Sheamle/notif
+
+-- --------------------------------------------
+-- Settings
+-- --------------------------------------------
+
+local body = {
+	-- Text
+	scale = 0.3,
+	offsetLine = 0.02,
+	-- Warp
+	offsetX = 0.005,
+	offsetY = 0.004,
+	-- Sprite
+	dict = 'commonmenu',
+	sprite = 'gradient_bgd',
+	width = 0.14,
+	height = 0.012,
+	heading = -90.0,
+	-- Betwenn != notifications
+	gap = 0.002,
+}
+
+local defaultText = '~r~~h~ERROR : ~h~~s~The text of the notification is nil.'
+local defaultType = 'topRight'
+local defaultTimeout = 6000
+
+RequestStreamedTextureDict(body.dict) -- Load the sprite dict. properly
+
+-- --------------------------------------------
+-- Calculus functions
+-- --------------------------------------------
+
+local function goDown(v, id) -- Notifications will go under the previous notifications
+	for i = 1, #v do
+		if v[i].draw and i ~= id then
+			v[i].y = v[i].y + (body.height + (v[id].lines*2 + 1)*body.offsetLine)/2 + body.gap
+		end
+	end
+end
+
+local function goUp(v, id) -- Notifications will go above the previous notifications
+	for i = 1, #v do
+		if v[i].draw and i ~= id then
+			v[i].y = v[i].y - (body.height + (v[id].lines*2 + 1)*body.offsetLine)/2 - body.gap
+		end
+	end
+end
+
+local function centeredDown(v, id) -- Notification will stay centered from the default position and new notification will go at the bottom
+	for i = 1, #v do
+		if v[i].draw and i ~= id then
+			v[i].y = v[i].y - (body.height + (v[id].lines*2 + 1)*body.offsetLine)/4 - body.gap/2
+			v[id].y = v[i].y + (body.height + (v[id].lines*2 + 1)*body.offsetLine)/2 + body.gap
+		end
+	end
+end
+
+local function centeredUp(v, id) -- Notification will stay centered from the default position and new notification will go at the top
+	for i = 1, #v do
+		if v[i].draw and i ~= id then
+			v[i].y = v[i].y + (body.height + (v[id].lines*2 + 1)*body.offsetLine)/4 + body.gap/2
+			v[id].y = v[i].y - (body.height + (v[id].lines*2 + 1)*body.offsetLine)/2 - body.gap
+		end
+	end
+end
+
+local function CountLines(v, text)
+	BeginTextCommandLineCount("STRING")
+    SetTextScale(body.scale, body.scale)
+    SetTextWrap(v.x, v.x + body.width - body.offsetX)
+	AddTextComponentSubstringPlayerName(text)
+	local nbrLines = GetTextScreenLineCount(v.x + body.offsetX, v.y + body.offsetY)
+	return nbrLines
+end
+
+local function DrawText(v, text)
+	SetTextScale(body.scale, body.scale)
+    SetTextWrap(v.x, v.x + body.width - body.offsetX)
+
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    EndTextCommandDisplayText(v.x + body.offsetX, v.y + body.offsetY)
+end
+
+local function DrawBackground(v)
+	DrawSprite(body.dict, body.sprite, v.x + body.width/2, v.y + (body.height + v.lines*body.offsetLine)/2, body.width, body.height + v.lines*body.offsetLine, body.heading, 255, 255, 255, 255)
+end
+
+-- --------------------------------------------
+-- Different options
+-- --------------------------------------------
+
+local positions = {
+	['centerRight'] = { x = 0.85, y = 0.5, notif = {}, offset = centeredUp },
+	['centerLeft'] = { x = 0.01, y = 0.5, notif = {}, offset = centeredUp },
+	['topRight'] = { x = 0.85, y = 0.015, notif = {}, offset = goDown },
+	['topLeft'] = { x = 0.01, y = 0.015, notif = {}, offset = goDown },
+	['bottomRight'] = { x = 0.85, y = 0.955, notif = {}, offset = goUp },
+	['bottomLeft'] = { x = 0.015, y = 0.75, notif = {}, offset = goUp },
+	-- ['position name'] = { starting x, starting y, notif = {} (nothing toput here it's juste the handle), offset = the way multiple notifications will stack up}
+}
+
+-- --------------------------------------------
+-- Main
+-- --------------------------------------------
+
+function SendNotification(options)
+	local text = options.text or defaultText
+	local type = options.type or defaultType
+	local timeout = options.timeout or defaultTimeout
+
+	local p = positions[type]
+	local id = #p.notif + 1
+	local nbrLines = CountLines(p, text)
+
+	p.notif[id] = {
+		x = p.x,
+		y = p.y,
+		lines = nbrLines, 
+		draw = true,
+	}
+
+	if id > 1 then
+		p.offset(p.notif, id)
+	end
+
+	Citizen.CreateThread(function()
+		Wait(timeout)
+		p.notif[id].draw = false
+	end)
+
+	Citizen.CreateThread(function()
+		while p.notif[id].draw do
+			Wait(0)
+			DrawBackground(p.notif[id])
+			DrawText(p.notif[id], text)
+		end
+	end)
+end
 
 -- Register Command and deal with 911 Resources
 RegisterCommand('911',function(source,args)
@@ -23,7 +165,6 @@ RegisterCommand('911',function(source,args)
     local playerPed = PlayerPedId(-1)
     local coords = GetEntityCoords(playerPed)
     local locationStreet = GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(playerPed))))
-    print("Preparing Call for Player Located At " .. locationStreet)
     for index, argument in pairs(args) do
         table.insert(strings,index,argument)
     end
@@ -32,8 +173,10 @@ RegisterCommand('911',function(source,args)
     if string.find(authoritiesCalled,"police") or string.find(authoritiesCalled,"ems") or string.find(authoritiesCalled,"fire")  then
         local data = {authorities = authoritiesCalled, location = locationStreet, reason = callReason, coords = {x = coords.x, y = coords.y, z = coords.z}}
         TriggerServerEvent("call911", playerPed, data)
-        TriggerEvent('chat:addMessage',{
-            args = {'Dialling 911 to request '.. authoritiesCalled}
+        SendNotification({
+            text = string.upper(authoritiesCalled) .." has been called, remain at your current location!",
+            type = "bottomRight",
+            timeout = 15000,
         })
     else
         TriggerEvent('chat:addMessage',{
